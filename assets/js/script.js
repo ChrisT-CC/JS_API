2123840d59ba85795d1d7bbdfdcd946801c7ab19
ChrisT-CC <cristaecc@gmail.com>
2021-12-08 23:25:16 +0000

    Add a function to post a request to the API

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,34 @@ const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
 document.getElementById("status").addEventListener("click", e => getStatus(e));
+document.getElementById("submit").addEventListener("click", e => postForm(e));
 
+/**
+ * Post a request to the API
+ */
+async function postForm(e) {
+    const form = new FormData(document.getElementById("checksform"));
+
+    const response = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+            "Authorization": API_KEY,
+        },
+        body: form
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+        console.log(data);
+    } else {
+        throw new Error(data.error);
+    }
+}
+
+/**
+ * Checks the API Key status if "Check Key" button is clicked
+ */
 async function getStatus(e) {
     const queryString = `${API_URL}?api_key=${API_KEY}`;
 
@@ -18,6 +45,9 @@ async function getStatus(e) {
     }
 }
 
+/**
+ * Displays API's key status in a modal
+ */
 function displayStatus(data) {
     let heading = "API Key Status";
     let results = `<div>Your key is valid until</div>`;
